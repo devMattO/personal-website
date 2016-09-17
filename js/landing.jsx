0ebed3d4fe3b9c3c5ac7b11bf7124589a84b58ca
Matt Olsen <olsen_matthew@yahoo.com>
2016-09-16 21:09:24 -1000

    setting up react and webpack

---
+++ js/landing.jsx
@@ -0,0 +1,12 @@
+const React = require('react')
+const { Link } = require('react-router')
+
+const Landing = () => (
+  <div className='app-container'>
+    <div className='content-div'>
+      <h1 className='title'>{ Matt (devmatto) Olsen }</h1>
+    </div>
+  </div>
+)
+
+module.exports = Landing
