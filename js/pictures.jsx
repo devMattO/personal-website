b1b3d9a60ea3aabb4b424f7d3fcc0bcaf3ed4f34
Matt Olsen <olsen_matthew@yahoo.com>
2016-09-27 14:48:46 -1000

    working on pictures page

--- js/pictures.jsx
+++ js/pictures.jsx
@@ -1,13 +1,18 @@
 const React = require('react')
+const Header = require('./header')
 
 const Pictures = React.createClass({
   render () {
     return (
-      <div className='pictures-content-div'>
-        <h1 className='pictures-header'>Matt Olsen</h1>
-        <div className='pics-div'>
-          <img src='./public/images/beach-cover.png' className='pictures' />
-          <img src='./public/images/caaarve.png' className='pictures' />
+      <div className='app-container'>
+        <div className='header-div'>
+          <Header />
+        </div>
+        <div className='pictures-content-div'>
+          <div className='pics-div'>
+            <img src='./public/images/beach-cover.png' className='pictures' />
+            <img src='./public/images/caaarve.png' className='pictures' />
+          </div>
         </div>
       </div>
     )
