7751add58f2c8fb9d94de00b43109e1b83ba13d4
Matt Olsen <olsen_matthew@yahoo.com>
2016-09-29 13:56:46 -1000

    writing methods handling input from form

--- js/contact.jsx
+++ js/contact.jsx
@@ -22,6 +22,18 @@ const Contact = React.createClass({
   handleMailBody: function(e) {
     this.setState({mailBody: e.target.value});
   },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var senderName = this.state.senderName.trim();
+    var senderEmail = this.state.senderEmail.trim();
+    var mailSubject = this.state.mailSubject.trim();
+    var mailBody = this.state.mailBody.trim();
+    if (!senderName || !senderEmail || !mailSubject || !mailBody)) {
+      return;
+    }
+    // TODO: send request to the server
+    this.setState({author: '', text: ''});
+  },
   render () {
     return (
       <div className='app-container'>
@@ -29,12 +41,12 @@ const Contact = React.createClass({
           <Header />
         </div>
         <div className='form-div'>
-          <form className='contact-form' onSubmit='' >
+          <form className='contact-form' onSubmit={handleSubmit} >
             <div className='contact-title'><h1 className='LAM-header'>Contact</h1></div>
-            <input className='contact-input' value={this.state.senderName} onChange={handleSenderName} type='text' name='sender-name' placeholder='Name' />
-            <input className='contact-input' value={this.state.senderEmail} onChange={handleEmailSender} type='text' name='email' placeholder='YourEmail@address.com' />
-            <input className='contact-input' value={this.state.mailSubject} onChange={handleMailSubject} type='text' name='mail-subject' placeholder='Subject' />
-            <textarea type='text' rows="4" cols="50" value={this.state.mailBody} onChange={handleMailBody} name="mail-body" placeholder='Enter text here...'></textarea>
+            <input className='contact-input' value={this.state.senderName} onChange={this.handleSenderName} type='text' name='sender-name' placeholder='Name' />
+            <input className='contact-input' value={this.state.senderEmail} onChange={this.handleEmailSender} type='text' name='email' placeholder='YourEmail@address.com' />
+            <input className='contact-input' value={this.state.mailSubject} onChange={this.handleMailSubject} type='text' name='mail-subject' placeholder='Subject' />
+            <textarea type='text' rows="4" cols="50" value={this.state.mailBody} onChange={this.handleMailBody} name="mail-body" placeholder='Enter text here...'></textarea>
             <input type='submit' value='Post' />
           </form>
         </div>
