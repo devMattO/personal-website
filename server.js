013253740c3774647de4584f8a055f78f35bfd7f
Matt Olsen <olsen_matthew@yahoo.com>
2016-10-03 22:44:58 -1000

    added webpack prod config

--- server.js
+++ server.js
@@ -11,30 +11,6 @@ const webpackConfig = require('./webpack.config.js')
 const compiler = webpack(webpackConfig)
 const bodyParser = require('body-parser')
 
-
-app.post('/#/contact/contact-me', (req,res)=>{
-  console.log(req)
-  var helper = require('sendgrid').mail;
-  var from_email = new helper.Email('test@example.com');
-  var to_email = new helper.Email('olsen_matthew@yahoo.com');
-  var subject = 'Hello World from the SendGrid Node.js Library!';
-  var content = new helper.Content('text/plain', 'Hello, Email!');
-  var mail = new helper.Mail(from_email, subject, to_email, content);
-
-  var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-  var request = sg.emptyRequest({
-    method: 'POST',
-    path: '/v3/mail/send',
-    body: mail.toJSON(),
-  });
-
-  sg.API(request, function(error, response) {
-    console.log(response.statusCode);
-    console.log(response.body);
-    console.log(response.headers);
-  });
-})
-
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(webpackDevMiddleware(compiler, {
@@ -55,8 +31,7 @@ const onStart = (err) => {
   }
   console.info(
     `==> 🌎 Listening on port ${port}. ` +
-    `Open up http://localhost:${port}/ in your browser.` +
-    encodeURIComponent('olsen_matthew@yahoo.com')
+    `Open up http://localhost:${port}/ in your browser.`
   )
 }
 
---
+++ webpack.config.prod.js
@@ -0,0 +1,136 @@
+/*eslint-disable */
+const webpack           = require("webpack");
+const path              = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const autoprefixer      = require("autoprefixer");
+
+module.exports = {
+
+  entry: {
+    main: path.join(__dirname, "./js/app.jsx")
+  },
+
+  output: {
+    path:       path.join(__dirname, "dist"),
+    filename:   "[name].js",
+    publicPath: "/"
+  },
+
+  module: {
+    loaders: [
+      {
+        test:    /\.(js|jsx)$/,
+        include: path.join(__dirname, "js"),
+        loaders: [
+          "babel"
+        ],
+      },
+
+      {
+        test:    /\.scss$/,
+        loaders: [
+          "style",
+          "css",
+          "sass"
+        ]
+      },
+
+      {
+        test:    /\.css$/,
+        loaders: [
+          "style",
+          "css",
+        ]
+      },
+
+      {
+        test:    /\.json$/,
+        loaders: [
+          "json"
+        ]
+      },
+
+      {
+        test:    /\.(png|jpg|gif)$/,
+        loaders: [
+          "url?limit=50000"
+        ]
+      },
+
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      },
+
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      },
+
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/octet-stream"
+      },
+
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file"
+      },
+
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=image/svg+xml"
+      }
+    ]
+  },
+
+  postcss: [ autoprefixer({ browsers: ["last 2 versions"] }) ],
+
+  resolve: {
+    extensions: ["", ".js", ".jsx", ".css", ".scss", ".less"],
+    root:       [
+      path.join(__dirname, "src", "javascripts"),
+      path.join(__dirname, "src", "images")
+    ],
+    modulesDirectories: ["node_modules"]
+  },
+
+  devtool: false,
+  // devtool: "#source-map",
+  stats:   {
+    colors: true
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        "NODE_ENV": JSON.stringify("production")
+      },
+      "API_ENDPOINT": process.env.npm_package_config_apiEndpoint
+    }),
+
+    new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
+
+    new webpack.ProvidePlugin({
+      jQuery: "jquery",
+      _:      "underscore"
+    }),
+
+    new webpack.optimize.OccurrenceOrderPlugin(),
+
+    new HtmlWebpackPlugin({
+      template: "./index.tpl.html"
+    }),
+
+    new webpack.optimize.DedupePlugin(),
+
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+
+    new webpack.optimize.AggressiveMergingPlugin()
+  ]
+
+};
