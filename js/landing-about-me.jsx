f2e8f3d4ad9fc76845ab2ca7ee331ffad8678f24
Matt Olsen <olsen_matthew@yahoo.com>
2016-10-05 14:11:13 -1000

    updated about me

--- js/landing-about-me.jsx
+++ js/landing-about-me.jsx
@@ -10,7 +10,7 @@ const LandingAboutMe = React.createClass({
         <h1 className='LAM-header-desc'>Full-Stack JavaScript Developer | Honolulu, HI</h1>
         <div className='LAM-div'>
           <p className='LAM-text'>
-            Thanks so much for checking out my web app! A few technical details: I built this in React and some other technologies used were Webpack, Sass, and Express. One random fact about me: I enjoy public speaking even though it scares the shit out of me.
+            Thanks so much for checking out my web app! I built this in React and some other technologies used were Webpack, Sass, and Express.
           </p><br />
           <p className='LAM-text'>
             I am a passionate developer who enjoys building fast, responsive, and powerful websites. I am currently searching for a team that can help me grow as a developer while I can contribute my skills to the overall goal. I have the burning desire to persevere through these challenges and have my work live up to my obsessive, pixel-pushing standards.
@@ -18,8 +18,8 @@ const LandingAboutMe = React.createClass({
           </p><br />
           <p className='LAM-text'>
             I have worked on pair-programming and group projects with teams that had to deliver live code within a short deadline.
-            I presented my team's final project at a State of Hawaii Hackathon (HACC) with over 350 people in attendance.
-            DevLeague was well presented and the Governor Ige of HI and his tech team where present.
+            I presented one of my team's project at a State of Hawaii Hackathon (HACC) with over 350 people in attendance.
+            DevLeague was well presented and the Governor Ige of HI and his tech team were present.
           </p><br />
           <div className='techProfDiv'>
             <h1 className='techProfHeader'>Technical Proficiencies:</h1>
